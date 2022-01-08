868bc90759877536f0cc22e341599eb6390a13d7
Gabriel F Guimaraes <ggfto@outlook.com>
2022-01-08 09:18:36 -0300

    update parcial

--- public/main.js
+++ public/main.js
@@ -1,9 +1,3 @@
-$(document).ready(function() {
-	document.onkeypress = function(data){
-		if (data.which == 13) gerarLinks();
-	};
-});
-
 function gerarLinks() {
     let arg1 = $('#os').val();
     let arg2 = $('#versoes').val();
@@ -16,13 +10,13 @@ function gerarLinksGTM() {
     generateLink(arg1, arg2, true);
 }
 
-function generateLink(os, versoes, gtm) {
+function generateLink(os, versoes) {
     let arrVersoes = versoes.split(',');
 	for(let i=0;i<arrVersoes.length;i++) {
-		let originBranch = ((arrVersoes[i] == 'master') ? 'trunk' : arrVersoes[i]).replace("-RC","").replace("-DEV","").replace("-HOMOLOG", "").replace(" ", "");
-		let destinationBranch = arrVersoes[i].toUpperCase();
-		if(gtm) originBranch += "-DEV";
-		if(gtm) destinationBranch += "-DEV";
+		let originBranch = arrVersoes[i].replace("-RC","").replace("-DEV","").replace("-HOMOLOG", "").replace(" ", "");
+		let destinationBranch = arrVersoes[i].toUpperCase().replace("-RC","").replace("-DEV","").replace("-HOMOLOG", "").replace(" ", "");
+		if($('#gtm').is(":checked")) originBranch += "-DEV";
+		if($('#gtm').is(":checked")) destinationBranch += "-DEV";
 
 		window.open(`https://git.sankhya.com.br/plataforma-w/sankhyaw/-/merge_requests/new?merge_request%5Bsource_branch%5D=${os}-${originBranch}
 			&merge_request%5Bsource_project_id%5D=838&merge_request%5Btarget_branch%5D=${destinationBranch}
@@ -30,10 +24,27 @@ function generateLink(os, versoes, gtm) {
 	}
 }
 
+function versionChange() {
+	let selName = "selGitVersion";
+	let versoes = $('#versoes').val();
+	$(`#${selName}`).html("");
+	let arrVersoes = versoes.split(',');
+	addOption(selName, "Selecione", undefined);
+	for(let i=0;i<arrVersoes.length;i++) {
+		addOption(selName, arrVersoes[i], arrVersoes[i]);
+	}
+}
+
+function addOption(select, optText, optVal) {
+	let o = new Option(optText, optVal);
+	$(o).html(optText);
+	$(`#${select}`).append(o);
+}
+
 function abrirGit() {
 	let os = $('#os').val();
-	if(os == undefined || os == null || os === "") os = $('#os-gtm').val();
-    let version = $('#version').val();
+	let selName = "selGitVersion";
+    let version = $(`#${selName}`).find(":selected").text();
     if(version != undefined) version = version.trim();
     if(version == 'master') version = 'trunk';
 	window.open(`https://git.sankhya.com.br/plataforma-w/sankhyaw/-/tree/${os}-${version.toUpperCase()}`, '_blank');
